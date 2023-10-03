4da5b4ff7f4b611ffe990e34731a8c1e65c5989e
Herlandro Tribiakto <herlandrotri@gmail.com>
2023-10-03 18:39:35 +0700

    prepare for testing deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app:{
+    baseURL: "/cara-instan-nx/",
+    buildAssetsDir: "assets",
+  },
   modules: ['@nuxt/content'],
   ssr: false,
   typescript: {
